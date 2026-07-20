699705ac5ff051a9d6a8632ba5e8ea051eb6d27c
kuseojin81 <sxozin009@gmail.com>
2026-07-20 10:34:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("AI 챗봇 만들기")
-st.header("오늘의 질문")
-st.subheader("입력 안내")
-st.write("질문을 자유롭게 입력하세요.")
+st.markdown("# AI 챗봇 만들기")
+st.markdown("---")
+st.markdown("## 질문을 하시면 AI 친구가 응답합니다.")
+st.markdown("* **제작 언어**: 'Python'")
+st.markdown("* **프레임워크**: 'streamlit'")
+st.markdown("### '최근 대화'")
+st.markdown("~~질문을 자유롭게 입력하세요.~~")
