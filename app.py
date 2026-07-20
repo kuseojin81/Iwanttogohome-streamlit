7941b62237f84e42a15f12e169e52832719717bf
kuseojin81 <sxozin009@gmail.com>
2026-07-20 11:24:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ age = st.number_input("반", min_value=1, max_value=100, value=1)
 ai_speed = st.select_slider("난이도",options=["매우 쉬움", "쉬움", "보통", "어려움", "매우 어려움"],value="보통")
 creativity = st.slider("점수", 0, 100, 50)
 question = st.text_area("소감", placeholder="소감입니다.")
-
+if st.button("확인")
        
  
